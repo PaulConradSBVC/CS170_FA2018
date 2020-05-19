5110874e1eeffadb072f5ac4350c64182a373a99
Paul Conrad <pjc@pauljconrad.net>
2020-05-18 19:45:33 -0700

    pwm_demo using physical pin numbering system

---
+++ hardware/pwm/pwm_demo_phys.s
@@ -0,0 +1,62 @@
+// assemble: g++ pwm_demo.s -lwiringPi -g -o pwm_mode
+
+.equ INPUT, 0
+.equ OUTPUT, 1
+.equ PWM_OUTPUT, 2
+
+.equ LOW, 0
+.equ HIGH, 1
+
+.equ LED_PIN, 12	// physical pin 12
+
+.equ PAUSE, 75		// 75 ms delay = 0.075 second delay
+
+.equ DELTA, 16		// delta increment/decrement (use a power of two that is a multiple of 1024)
+
+.global main
+
+.align 4
+.section .rodata
+output_msg:	.asciz	"Outputting %d to pin #%d\n"
+
+.align 4
+.text
+main:
+	push {R4, LR}
+	bl wiringPiSetupPhys
+
+	// set up pin mode
+	mov R0, #LED_PIN
+	mov R1, #PWM_OUTPUT
+	bl pinMode
+
+	// run our loop
+	mov R4, #0
+do_while:
+	mov R0, #LED_PIN
+	mov R1, R4
+	bl pwmWrite
+
+	// delay
+	mov R0, #PAUSE
+	bl delay
+
+	add R4, #DELTA
+	cmp R4, #1024
+	ble do_while
+
+do_while2:
+	mov R0, #LED_PIN
+	mov R1, R4
+	bl pwmWrite
+
+	// delay
+	mov R0, #PAUSE
+	bl delay
+
+	sub R4, #DELTA
+	cmp R4, #0
+	bgt do_while2
+
+	mov R0, #0
+	pop {R4, PC}
